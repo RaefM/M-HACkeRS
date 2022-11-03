965b51c13a5275ae98c867b16a62bf48dec1951d
Ubuntu <ubuntu@ip-172-31-29-176.ec2.internal>
2022-11-03 15:03:12 +0000

    populateDB update

--- identisoundd/populateDB.py
+++ identisoundd/populateDB.py
@@ -16,24 +16,40 @@ cursor.execute('ALTER SEQUENCE songs_id_seq RESTART;')
 with open("../movieData.txt", "r") as file:
     contents = file.readlines()
 
-for x in range(1, 4):
+for x in range(65, 68):
+    print(contents[x])
     movieInfo = contents[x].split(';')
     title = movieInfo[0]
     title = title.rstrip()
     songInfo = movieInfo[2]
     songs = songInfo.split("%")
     songs = songs[:-1]
-    cursor.execute("INSERT INTO movies (name) VALUES(%s);", (title,))
+    cursor.execute("INSERT INTO movies (name) VALUES(%s) RETURNING id;", (title,))
+    movieID = cursor.fetchall()[0][0]
     for song in songs:
-        songName, artist = song.split("$") 
+        songName, artist = song.split("$") #Ty Dolla $ign messes this up 
+        print(songName) 
         songName = songName.rstrip()
+        if len(songName) == 0:
+            continue
         artist = artist.rstrip()
-        cursor.execute("INSERT INTO songs (name, artistinfo) VALUES(%s, %s);", (songName, artist,))
+        cursor.execute("SELECT id FROM songs WHERE name=%s;", (songName,))
+        foundSong = cursor.fetchall()
+        if len(foundSong) == 0:
+            cursor.execute("INSERT INTO songs (name, artistinfo) VALUES(%s, %s) RETURNING id;", (songName, artist,))
+            songID = cursor.fetchall()[0][0]
+        else:
+            songID = foundSong[0][0]
+        cursor.execute("INSERT INTO songs_to_movies (songid, movieid) VALUES(%s, %s);", (songID, movieID,))
 
 cursor.execute('SELECT * FROM movies;')
 rows = cursor.fetchall()
 print(rows)
 
 cursor.execute('SELECT * FROM songs;')
+rows = cursor.fetchall()
+print(rows)
+
+cursor.execute('SELECT * FROM songs_to_movies;')
 rows = cursor.fetchall()
 print(rows)
