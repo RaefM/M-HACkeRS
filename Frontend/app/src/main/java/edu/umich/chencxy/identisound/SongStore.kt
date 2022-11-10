4d2730ea6a44688d4177b5bd8e222e5c17ff89d7
Caitlyn-Xinyi <chencxy@umich.edu>
2022-11-09 23:47:09 -0500

    update

--- Frontend/app/src/main/java/edu/umich/chencxy/identisound/SongStore.kt
+++ Frontend/app/src/main/java/edu/umich/chencxy/identisound/SongStore.kt
@@ -7,8 +7,12 @@ import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley.newRequestQueue
+import com.shazam.shazamkit.AudioSampleRateInHz
+import com.shazam.shazamkit.ShazamKit
+import com.shazam.shazamkit.ShazamKitResult
 import org.json.JSONArray
 import org.json.JSONException
+//import libs.shazamkit-android-release.aar
 import org.json.JSONObject
 import kotlin.reflect.full.declaredMemberProperties
 
@@ -19,24 +23,37 @@ object SongStore {
 
     private lateinit var queue: RequestQueue
     private const val serverUrl = "https://54.226.221.81/"
-    fun postSong(context: Context, song: Song) {
-        val jsonObj = mapOf(
-            "name" to song.Songname,
-            "uri" to song.audio,
-        )
-        val postRequest = JsonObjectRequest(Request.Method.POST,
-            serverUrl+"postaudio/", JSONObject(jsonObj),
-            {
-                Log.d("postSong", "Song posted!")
-                getSongTitle(context)
-            },
-            { error -> Log.e("postSong", error.localizedMessage ?: "JsonObjectRequest error") }
-        )
+//    fun postSong(context: Context, song: Song) {
+//        val jsonObj = mapOf(
+//            "name" to song.Songname,
+//            "uri" to song.audio,
+//        )
+//        val postRequest = JsonObjectRequest(Request.Method.POST,
+//            serverUrl+"postaudio/", JSONObject(jsonObj),
+//            {
+//                Log.d("postSong", "Song posted!")
+//                getSongTitle(context)
+//            },
+//            { error -> Log.e("postSong", error.localizedMessage ?: "JsonObjectRequest error") }
+//        )
+//
+//        if (!this::queue.isInitialized) {
+//            queue = newRequestQueue(context)
+//        }
+//        queue.add(postRequest)
+//    }
 
-        if (!this::queue.isInitialized) {
-            queue = newRequestQueue(context)
-        }
-        queue.add(postRequest)
+    fun postSong(context: Context, song: Song) {
+
+        val signatureGenerator = (ShazamKit.createSignatureGenerator(AudioSampleRateInHz.SAMPLE_RATE_48000) as ShazamKitResult.Success).data
+
+        signatureGenerator.append(song.audio.toByteArray(), song.audio.toByteArray().size, System.currentTimeMillis())
+        val signature = signatureGenerator.generateSignature()
+
+        val catalog = ShazamKit.createShazamCatalog(developerTokenProvider, selectedLocale.value)
+        val session = (ShazamKit.createSession(catalog) as ShazamKitResult.Success).data
+        val matchResult = session.match(signature)
+
     }
 
     fun getSongTitle(context: Context) {
